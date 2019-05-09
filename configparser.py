0e6bfb9633ad84adff752f4557024b6b558c2a4b
root <root@localhost.localdomain>
2019-05-09 20:34:53 +0200

    configparser v1.0.2

--- configparser.py
+++ configparser.py
@@ -1,8 +1,9 @@
 import string
 import re
 
-def main():
+globalcontainer = []
 
+def categorieslist():
     with open('.config','r') as f:
          res = ""
          categories = re.compile(r'(#\n)(# (.)*\n)(#\n)')
@@ -11,8 +12,23 @@ def main():
          out = open("output.txt", "a")
          for i in res:
              out.write(i[1])
+             globalcontainer.append(i[1])
          f.close()
          out.close()
+
+# def categoriescontent():
+#     with open('.config','r') as f:
+#          res = ""
+#          catcontent = re.compile(r'(#\n)(# (.)*\n)(#\n)(.)*(\n)\s$')
+#          contents = f.read()
+#          res = categories.findall(contents)
+#          for i in res:
+#              globalcontainer.append(i[1])
+#          f.close()
+#          out.close()
+
+def main():
+    categorieslist()
 
 
 if __name__=="__main__":
