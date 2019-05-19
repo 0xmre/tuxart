77691f672f94068107ec2cb2a12dce29e61bfd6d
KhaledArsa <khaled.arsalane@live.it>
2019-05-19 19:57:19 +0200

    -Creation de 3 .config differents pour test -dossier kernel linux -nouveau parser "temp.py" -script kconfig-diff.py pour evaluation differences
    	deleted:    .config
    	new file:   conf1/.config
    	new file:   conf2/.config
    	new file:   conf3/.config

--- configparser.py
+++ configparser.py
@@ -1,35 +1,67 @@
 import string
 import re
 
-globalcontainer = []
+globalcontainer = {}
 
 def categorieslist():
     with open('.config','r') as f:
          res = ""
-         categories = re.compile(r'(#\n)(# (.)*\n)(#\n)')
+         categories = re.compile(r'(#\n)(# (.)*)(\n#\n)')
          contents = f.read()
+         contents = re.sub("\( | \)", "\s", contents)
          res = categories.findall(contents)
+
          out = open("output.txt", "a")
          for i in res:
-             out.write(i[1])
-             globalcontainer.append(i[1])
+             out.write(i[1]+'\n')
+             globalcontainer[i[1]] = []
+             #globalcontainer.append(i[1])
          f.close()
          out.close()
 
-# def categoriescontent():
-#     with open('.config','r') as f:
-#          res = ""
-#          catcontent = re.compile(r'(#\n)(# (.)*\n)(#\n)(.)*(\n)\s$')
-#          contents = f.read()
-#          res = categories.findall(contents)
-#          for i in res:
-#              for y in i:
-#                 globalcontainer.append(i[1][y])
-#          f.close()
+def categoriescontent():
+    with open('.config','r') as f:
+         #res = ""
+         #catcontent = re.compile(r'(#\n)(# (.)*\n)(#\n)(.)*(\n)\s$')
+         contents = f.read()
+         #res = categories.findall(contents)
+         for key in globalcontainer:
+
+            print(key)
+            tmp = []
+            #pattern = r"^"+i+r"(.)*\n{0|1}"
+            #print(pattern)
+            #pattern2 = re.compile(r'(^'+globalcontainer[i]+')(.)*(\n{0|1})')
+            pattern2 = re.search(key + '(\n#\n)(.)*(\n)', contents)
+            if pattern2:
+                print("ok")
+            else:
+                print("Eh non")
+            #pattern2 = re.compile(r'(^'+key+')(.)*(\n{0|1})')
+
+            #option = pattern2.findall(contents)
+            #tmp.append(pattern2.string)
+            #for j in option:
+                 #tmp.append(j)
+                 #globalcontainer[i].append(j[1])
+                 #print(globalcontainer[i])
+                 #for y in subcat.group():
+                    #globalcontainer[i].append(y)
+            print(tmp)
+            globalcontainer[key] = tmp
+         f.close()
+
+def parseconfig():
+    with open('.config', 'r') as f:
+        contents = f.read()
+        res = re.split("((.)*(\n))*(\n)", contents)
+        print(len(res))
 
 def main():
-    categorieslist()
-
+    #categorieslist()
+    #categoriescontent()
+    #print(globalcontainer)
+    parseconfig()
 
 if __name__=="__main__":
     main()
