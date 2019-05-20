72fc37b877d93dc802ca3a314c176276c2026788
KhaledArsa <khaled.arsalane@live.it>
2019-05-20 17:42:35 +0200

    recup valuers dans temp.py

--- temp.py
+++ temp.py
@@ -11,29 +11,41 @@ from kconfiglib import Kconfig, Symbol, Choice, COMMENT, MENU, MenuNode, \
                        KconfigError
 
 def search_item(node):
-    i = 0
+    #i=0
     while node:
         if isinstance(node.item, Symbol):
             #node.kconfig.load_config()*
-            for menuitem in node.item.nodes:
-                #if menuitem.
-                print(menuitem)
-            #node.help()
 
+            sym = node.item
+            #if sym.str_value == "y":
+                #i+=1
+            print("\nvalue = " + sym.str_value)
+            #for menuitem in node.item.nodes:
+                #if menuitem.
+                #print(menuitem.sym)
+            #node.help()
         if node.list:
             search_item(node.list)
-        i = i+1
+
         node = node.next
+    #print(i)
 
 def main():
 
     kconf = Kconfig(sys.argv[1])
     search_item(kconf.top_node)
+
+
+
+
+
+
+
+
     #print(Kconfig.load_config("CONFIG_ETHERNET"))
     #print(IS_ENABLED("CONFIG_ETHERNET"))
 
     #kconf = Kconfig(sys.argv[1])
-    #search_items(kconf.top_node)
 
 
 
