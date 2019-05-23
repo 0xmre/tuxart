271565014b66376b547babbf6c32660249141ee3
Pouteau1 <pouteau.pierre1@gmail.com>
2019-05-23 17:03:31 +0200

    nouveau svg + update fonction
    Ajout d'un nouveau SVG à ne pas modifier, avec des balises rangées pour que mon script fonctionne bien
    Et premiere partie de ma fonction. Y a pas grand chose pour l'instant parce que je fais encore des tests, mais il n'y a qu'un seul truc qui fonctionne pas. Et quand j'aurai réussi je saurai (en théorie) changer la couleur de n'importe quelle partie du Tux)

--- tux_modifier.py
+++ tux_modifier.py
@@ -1,7 +1,31 @@
 import re
 
-regex = "paupiere"
+#left eye :
+#paupiere_gauche
+#oeil_gauche_reflet_interieur_pupille
+#pupille_gauche
+#oeil_gauche_blanc
 
-test_str = open('Tux.svg', 'r').read()
-matches = re.findall(regex, test_str)
-print(matches)
+
+
+#svgFile = open("original_tux.svg").read()
+svgFile = "id=\"paupiere_gauche\"style=\"fill:url(#radialGradient18832);fill-opacity:1;stroke:none\""
+regex = "paupiere_gauche"
+matches = re.split(regex, svgFile)
+test_str = matches[1]
+print(test_str)
+regex= "fill:"
+matches = re.split(regex, test_str, 1)
+test_str = matches[1]
+print(test_str)
+regex= ";"
+matches = re.split(regex, test_str, 1)
+fillToReplace = matches[0]
+print(fillToReplace)
+fillToReplace = re.escape(fillToReplace)
+newFill="#ff0000"
+newSvgFile = re.sub(fillToReplace, newFill, svgFile)
+print(newSvgFile)
+#f=open("tux_mod.svg")
+#f.write(newSvgFile)
+#f.close()
