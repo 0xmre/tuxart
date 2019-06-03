728108f3ffdfb5b6dae54aec95098b07e4095e2d
HommeOursPorc <eliotmarie0@gmail.com>
2019-06-03 15:58:12 +0200

    adding shell script, which is going to be the 'makefile'

---
+++ tuxart.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+  echo "pas de parametre"
+  for i in `seq 1 2`;
+  do
+    mkdir -p example$i
+    cd linux-4.13.3
+    make alldefconfig
+    cd ..
+    cp linux-4.13.3/.config example$i/.config
+  done
+else
+  echo "parametre : "$1
+fi
