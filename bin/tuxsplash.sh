2ffc07dd6fbd9830f01e313434d44f65cc294015
R-Faraji <50630899+R-Faraji@users.noreply.github.com>
2019-06-28 02:56:40 +0200

    Now takes .svg and .png input

--- bin/tuxsplash.sh
+++ bin/tuxsplash.sh
@@ -3,7 +3,7 @@
 OS=`lsb_release -si | tr '[:upper:]' '[:lower:]'`	#Retrieve name of OS release
 pip3pm=`which pip3`					#Retrieve path of pip3
 cairo=`which cairosvg`					#Retrieve path of cairosvg
-
+im=`which convert`					#Retrieve path of convert part of imagemagick
 
 declare -A osInfo;					#Retrieving default package manager
 osInfo[/etc/redhat-release]=yum
@@ -16,7 +16,6 @@ for f in ${!osInfo[@]}
 do
     if [[ -f $f ]];then
         var="${osInfo[$f]}"		
-	
     fi
 done
 
@@ -29,8 +28,8 @@ elif [ "$1" = "--help" ] || [ "$1" = "-h" ];		#Help
 then	
 	echo ""
 	echo "# To use script you have to be root usr : sudo su "
-	echo "# Format goes like this ./tuxsplash PATH/tux.svg " 
-	echo "# If you want to find your generated tux from tuxArt go to ~/Pictures" 
+	echo "# Format goes like this ./tuxsplash.sh /PATH/tux.png or .svg " 
+	echo "# If you want to splash your generated tux from tuxArt go to ~/Pictures" 
 	exit 1
 
 elif [ ! -f $1 ]					#Checking if file exist
@@ -78,14 +77,33 @@ then
 		fi
 	fi 
 	
+	if [ -z $im ]
+	then
+		
+		$var install imagemagick
+		
+	fi 
+
+
+	if [[ $1 == *.png ]]
+	then 
+		convert $1 -resize "140x165" -quality 100 $1
+		cd /usr/share/plymouth/themes/*-logo            
+		mv *-logo.png old-splash.png 
+		cp $1 ./$OS-logo.png
+		echo "This may take a few seconds please be patient and wait =)"
+		update-initramfs -u
+	fi
 	
-	cairosvg $1 -o ~/Pictures/tuxsplash.png		#Making png copy of svg tux in Picture directory
-	cd /usr/share/plymouth/themes/*-logo            
-	mv *-logo.png old-splash.png 
-	cp ~/Pictures/tuxsplash.png ./$OS-logo.png
-	echo "This may take a few seconds please be patient and wait =)"
-	update-initramfs -u
-	
+	if [[ $1 == *.svg ]]
+	then 	
+		cairosvg -s 0.50 $1 -o ~/Pictures/tuxsplash.png		#Making png copy of svg tux in Picture directory
+		cd /usr/share/plymouth/themes/*-logo            
+		mv *-logo.png old-splash.png 
+		cp ~/Pictures/tuxsplash.png ./$OS-logo.png
+		echo "This may take a few seconds please be patient and wait =)"
+		update-initramfs -u
+	fi
 
 fi
 
